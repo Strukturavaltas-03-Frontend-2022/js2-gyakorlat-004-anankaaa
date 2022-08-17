05bb49dabeaae45247effa673dd61d64dddb7c4e
Anankaaa <hegyaljai83@hotmail.com>
2022-08-17 17:32:48 +0200

    első és második feladat kész

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,12 @@
+const countOfWorkingDays = () => {
+    const startTime = new Date('January 1');
+    const endTime = new Date ('August 31');
+    let timeDifference = (endTime-startTime)/1000/60/60/24/(7/5);
+    return Math.round(timeDifference);
+}
 
 
 
 
 
-
-// export default countOfWorkingDays;
+export default countOfWorkingDays;
